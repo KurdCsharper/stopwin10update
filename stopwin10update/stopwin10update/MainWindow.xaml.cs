1457e10a43545ba60636fe664c471e3357e071a2
csharp007 <csharp007@users.noreply.github.com>
2018-09-30 22:07:03 +0330

    Works

--- stopwin10update/stopwin10update/MainWindow.xaml.cs
+++ stopwin10update/stopwin10update/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceProcess;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,9 +14,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
 namespace stopwin10update
 {
+
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -23,6 +26,79 @@ namespace stopwin10update
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                StopService("WinDefend");
+
+            }
+            catch (Exception er)
+            {
+
+                MessageBox.Show(er.Message.ToString());
+            }
+
+        }
+        public static ServiceController GetService(string serviceName)
+        {
+            ServiceController[] services = ServiceController.GetServices();
+            foreach (var item in services)
+            {
+                if (item.ServiceName.Equals(serviceName))
+                {
+                    return item;
+                }
+            }
+            return services[0];
+        }
+        public static bool IsServiceRunning(string serviceName)
+        {
+            ServiceControllerStatus status;
+            uint counter = 0;
+            do
+            {
+                ServiceController service = GetService(serviceName);
+                if (service == null)
+                {
+                    return false;
+                }
+
+                Thread.Sleep(100);
+                status = service.Status;
+            } while (!(status == ServiceControllerStatus.Stopped ||
+                       status == ServiceControllerStatus.Running) &&
+                     (++counter < 30));
+            return status == ServiceControllerStatus.Running;
+        }
+
+        public static bool IsServiceInstalled(string serviceName)
+        {
+            return GetService(serviceName) != null;
+        }
+
+        public static void StartService(string serviceName)
+        {
+            ServiceController controller = GetService(serviceName);
+            if (controller == null)
+            {
+                return;
+            }
+
+            controller.Start();
+            controller.WaitForStatus(ServiceControllerStatus.Running);
+        }
+        public static void StopService(string serviceName)
+        {
+            ServiceController controller = GetService("WinDefend");
+            if (controller == null)
+            {
+                return;
+            }
+            controller.Stop();
+            controller.WaitForStatus(ServiceControllerStatus.Stopped);
         }
     }
 }
